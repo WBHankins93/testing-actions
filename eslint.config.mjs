55c5de69ea3d0183a511b101c75efaa38c5ba80d
Ben Hankins <benhankins@Bens-MBP.lan>
2025-01-10 00:09:24 -0600

    Update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    rules: {
+      'constructor-super': 'error',
+      'no-const-assign': 'error',
+      'no-this-before-super': 'error',
+      'no-undef': 'error',
+      'no-unreachable': 'error',
+      'no-unused-vars': 'warn',
+      'no-use-before-define': 'error'
+    }
+  },
   pluginJs.configs.recommended,
 ];
